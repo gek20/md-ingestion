b291b75bf8f912af57deddbec4fa3128b944882c
seolih <martens@dkrz.de>
2023-02-09 19:42:14 +0100

    mini changes

--- mdingestion/community/nilu.py
+++ mdingestion/community/nilu.py
@@ -16,3 +16,5 @@ class NiluIso19139(Repository):
     DESCRIPTION = """NILU, the Norwegian Institute for Air Research is an independent, nonprofit institution established in 1969. Through its research NILU increases the understanding of processes and effects of climate change, of the composition of the atmosphere, of air quality and of hazardous substances. Based on its research, NILU markets integrated services and products within the analytical, monitoring and consulting sectors. NILU is concerned with increasing public awareness about climate change and environmental pollution. NILUs near 200 researchers, technicians and other experts are primarily commissioned by the Research Council of Norway and by Norwegian and international industry and government agencies. The institute takes an active part in the EUs research programs.
     """
     LINK = 'https://www.nilu.no/'
+    REPOSITORY_ID = ''
+    REPOSITORY_NAME = ''
