11887c792409b6f1df9602ecb86ccd20fce85aa6
seolih <martens@dkrz.de>
2023-06-26 14:25:29 +0000

    adding additional harvesting endpoint

--- mdingestion/community/b2share_summerschool.py
+++ mdingestion/community/b2share_summerschool.py
@@ -2,14 +2,9 @@ from .base import Repository
 from ..service_types import SchemaType, ServiceType
 
 
-class WhateverinEudatcore(Repository):
+class BaseSummerschool(Repository):
     IDENTIFIER = 'summerschool'
     TITLE = 'Summer School Kajaani'
-    URL = 'https://vm0897.kaj.pouta.csc.fi/api/oai2d'
-    SCHEMA = SchemaType.Eudatcore
-    SERVICE_TYPE = ServiceType.OAI
-    OAI_METADATA_PREFIX = 'eudatcore'
-    OAI_SET = '	e9b9792e-79fb-4b07-b6b4-b9c2bd06d095'
     PRODUCTIVE = False
     DATE = '2023-06-27'
     CRON_DAILY = False
@@ -24,9 +19,29 @@ class WhateverinEudatcore(Repository):
             doc.publisher = 'EUDAT B2SHARE'
         if not doc.publication_year:
             doc.publication_year = self.find('header.datestamp')
+
+
+class SummerschoolEudat(BaseSummerschool):  
+    IDENTIFIER = 'summerschool_eudat'
+    URL = 'https://vm0897.kaj.pouta.csc.fi/api/oai2d'
+    SCHEMA = SchemaType.Eudatcore
+    SERVICE_TYPE = ServiceType.OAI
+    OAI_METADATA_PREFIX = 'eudatcore'
+    OAI_SET = 'e9b9792e-79fb-4b07-b6b4-b9c2bd06d095'
+
+    def update(self, doc):
         if not doc.resource_type:
             doc.resource_type = 'Dataset'
         if not doc.discipline:
             doc.discipline = 'Social/Natural Sciences'
         if not doc.contact:
             doc.contact = 'somecrazyemail@coolrepo.eu, www.anyhelpdesk.eu'
+
+
+class KajaaniSummerschool(BaseSummerschool):  
+    IDENTIFIER = 'summerschool_2023'
+    URL = 'https://vm0897.kaj.pouta.csc.fi/api/oai2d'
+    SCHEMA = SchemaType.Eudatcore
+    SERVICE_TYPE = ServiceType.OAI
+    OAI_METADATA_PREFIX = 'eudatcore'
+    OAI_SET = '06acf21c-7e65-4360-8306-62b781121096'
